900709a9be3425e8e680e1b539e50394a8c648cd
aleksandraaa111 <aleksandravukasinovic017@gmail.com>
2025-07-08 11:00:15 +0200

    a and b initial code

--- vezbeGit/main.c
+++ vezbeGit/main.c
@@ -1,5 +1,14 @@
-#include<stdio.h>
-int main(){
-	printf("Hello world!\n");
+#include <stdio.h>
+ 
+int main() {
+    int a = 5;
+	int b = 4;
+	int c;
+ 
+	if (a > b)
+		c = a - b;
+	else 
+		c = b - a;
+	printf("Rezultat je: %d \n", c);
 	return 0;
 }
